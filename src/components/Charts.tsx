1a78f6f9147ced058978e265c2fcda43eda0fd46
Aditya putra purnama <122659508+adityaputra99983@users.noreply.github.com>
2025-11-30 12:19:26 +0700

    Add files via upload

---
+++ src/components/Charts.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    ArcElement,
+    Tooltip,
+    Legend,
+} from "chart.js";
+
+import { Bar, Pie } from "react-chartjs-2";
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    ArcElement,
+    Tooltip,
+    Legend
+);
+
+export { Bar, Pie };
